bf9dfc6e4027bee1d6fd5c35339fe63e7925c39d
JameZ2714 <jamamir2714@students.uplifteducation.org>
2025-01-31 11:22:42 -0600

    Add files via upload

---
+++ SetAppointment.js
@@ -0,0 +1,34 @@
+(function(w, d, s, i) {
+        var script = d.createElement(s);
+        script.async = true;
+        script.src = "//widget.simplybook.me/v2/widget/widget.js";
+        script.onload = function() {
+        new SimplybookWidget({"widget_type":"iframe",
+            "url":"https:\/\/turbokart.simplybook.me",
+            "theme":"default",
+            "theme_settings":{"timeline_hide_unavailable":"1",
+            "hide_past_days":"0",
+            "timeline_show_end_time":"0",
+            "timeline_modern_display":"as_slots",
+            "sb_base_color":"#2e77ae",
+            "display_item_mode":"block",
+            "booking_nav_bg_color":"#2e77ae",
+            "body_bg_color":"#f2f2f2",
+            "sb_review_image":"1",
+            "sb_review_image_preview":"\/uploads\/turbokart\/image_files\/preview\/9e578a6861f7cf8152929b99ff5bfaac.png",
+            "dark_font_color":"#474747",
+            "light_font_color":"#f5fcff",
+            "btn_color_1":"#2e77ae",
+            "sb_company_label_color":"#ffffff",
+            "hide_img_mode":"1",
+            "show_sidebar":"1",
+            "sb_busy":"#c7b3b3",
+            "sb_available":"#d6ebff"},
+            "timeline":"modern",
+            "datepicker":"top_calendar",
+            "is_rtl":false,
+            "app_config":{"clear_session":0,"allow_switch_to_ada":0,
+            "predefined":[]},"container_id":"sbw_5a7bms"});
+        };
+        d.head.appendChild(script);
+        })(window, document, 'script', 'sbw_5a7bms');
